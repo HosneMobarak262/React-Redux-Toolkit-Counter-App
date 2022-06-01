8d9f52864246f7d39fcd548aa0fea83c49ae4044
Hosne Mobarak <85449416+HosneMobarak262@users.noreply.github.com>
2022-06-01 17:28:56 +0600

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import CounterView from './features/counter/CounterView';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Redux Toolkit Counter App</h1>
+      <CounterView />
     </div>
   );
 }
 
 export default App;
+
+
+//  npm install @reduxjs/toolkit react-redux
